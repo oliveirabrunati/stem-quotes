8e80a19c14d4b7b4bcf3f0f3f75fe6f9b378dd31
oliveirabrunati <oliveira_bruna@outlook.com>
2022-04-14 00:53:56 -0300

    transformando card em javascript

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -49,18 +49,40 @@ function criarCard(){
     artigo.className = "card";
     let divCabecalho = document.createElement("div");
     divCabecalho.className = "cabecalho";
-
+    // cria a tag img e adiciona a imagem
     let imagem = document.createElement("img");
     imagem.src = "img/steve-jobs.jpg";
-
+    // cria a tag header
     let hearderCard = document.createElement("header");
+    // cria a tag h2 e adiciona o nome do autor
     let tituloCard = document.createElement("h2");
     tituloCard.className = "titulo";
     let textoTituloCard = document.createTextNode("Steve Jobs");
-
+    // cria a tag span e adiciona o cargo do autor
     let tagSpan = document.createElement("span");
     let textoSpan = document.createTextNode("Fundador da Apple Inc.");
 
+    // cria o corpo
+    let divCorpo = document.createElement("div");
+    divCorpo.className = "conteudoCard";
+    // cria a tag paragráfo
+    let paragrafoCard = document.createElement("p");
+    let conteudoParagrafo = document.createTextNode("Tenha coragem de seguir o que seu coração e sua intuição dizem. Eles já sabem o que você realmente deseja. Todo resto é secundário.");
+
+    // cria o rodape
+    let divRodape = document.createElement("footer");
+    divRodape.className = "rodapeCard";
+    let linkBiografia = document.createElement("a");
+    let textoLink = document.createTextNode("Biografia");
+    // criando icones rodape
+    let divSocial = document.createElement("div");
+    divSocial.className = "icones";
+    let iconeCurtir = document.createElement("i");
+    iconeCurtir.className = "fas fa-solid fa-heart";
+    let iconeCompartilhar = document.createElement("i");
+    iconeCompartilhar.className = "fas fa-solid fa-share";
+    
+    
     divCabecalho.appendChild(imagem);
     divCabecalho.appendChild(hearderCard);
     hearderCard.appendChild(tituloCard);
@@ -69,7 +91,18 @@ function criarCard(){
     tagSpan.appendChild(textoSpan);
     artigo.appendChild(divCabecalho);
     grupoCards.appendChild(artigo);
+    
+    artigo.appendChild(divCorpo);
+    divCorpo.appendChild(paragrafoCard);
+    paragrafoCard.appendChild(conteudoParagrafo);
 
+    linkBiografia.appendChild(textoLink);
+    divRodape.appendChild(linkBiografia);
+    artigo.appendChild(divRodape);
+
+    divRodape.appendChild(divSocial);
+    divSocial.appendChild(iconeCurtir);
+    divSocial.appendChild(iconeCompartilhar);
 }
 
 criarCard();
